2f250087a060f3034a7349446b9b59870e40ddc4
koneru123 <koneru.priyanka@gmail.com>
2020-06-06 11:41:27 -0700

    Initial project set-up

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+let app = express();
+
+// middleware
+const parser = require('body-parser');
+
+app.use(express.static(__dirname + '/../client/dist'));
+app.use(parser());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+});
+
+const port = 3005;
+
+app.listen(port, () => console.log(`App listening on http://localhost:${port}`));
+
+module.exports = app;
+
