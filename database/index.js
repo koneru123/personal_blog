095bbb097ccd7a64b13434562c56c773c957d872
koneru123 <koneru.priyanka@gmail.com>
2020-06-06 14:42:12 -0700

    update to DB connection

--- database/index.js
+++ database/index.js
@@ -1,13 +1,14 @@
+require("dotenv").config();
 const mongoose = require('mongoose');
 
-const mongoURI = 'mongodb://localhost:27017/blogs';
+const MONGO_URI = process.env.MONGO_URI;
 
-const db = mongoose.connect(mongoURI, { useNewUrlParser: true });
+const db = mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 db
-  .then(db => console.log(`Connected to: ${mongoURI}`))
+  .then(db => console.log(`Connected to Database`))
   .catch(err => {
-    console.log(`There was a problem connecting to mongo at: ${mongoURI}`);
+    console.log(`There was a problem connecting to mongo at: ${MONGO_URI}`);
     console.log(err);
   });
 
